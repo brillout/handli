ac1c4ae908edc848fd2ceaa645a2f7181f751667
Romuald Brillout <git@brillout.com>
2024-03-14 19:49:39 +0100

    fix logo base url

--- demo/index.jsx
+++ demo/index.jsx
@@ -36,7 +36,12 @@ function Header() {
     />
   )
   const handliIcon = (
-    <img src="/logo.svg" height="40" style={{ verticalAlign: 'middle', marginTop: -3 }} alt="Handli" />
+    <img
+      src={import.meta.env.BASE_URL + 'logo.svg'}
+      height="40"
+      style={{ verticalAlign: 'middle', marginTop: -3 }}
+      alt="Handli"
+    />
   )
   return (
     <h1
